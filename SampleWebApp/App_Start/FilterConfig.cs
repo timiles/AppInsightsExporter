cf04ab7c315a03735ca73d7db4eb01ad2cd675e0
Tim Iles <tim_iles@live.com>
2016-03-26 13:43:43 +0000

    Log errors to App Insights via attribute

--- SampleWebApp/App_Start/FilterConfig.cs
+++ SampleWebApp/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using SampleWebApp.Attributes;
 
 namespace SampleWebApp
 {
@@ -8,6 +9,7 @@ namespace SampleWebApp
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AiHandleErrorAttribute());
         }
     }
 }
--- SampleWebApp/Controllers/HomeController.cs
+++ SampleWebApp/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace SampleWebApp.Controllers
     {
         public ActionResult Index()
         {
+            throw new ApplicationException("Test error");
             ViewBag.Title = "Home Page";
 
             return View();
