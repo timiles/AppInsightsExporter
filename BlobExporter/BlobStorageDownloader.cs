73aaffb9c3b8ab882ae0e88bc624d852fae09018
Tim Iles <tim_iles@live.com>
2016-03-27 14:01:38 +0100

    Connect to azure blob storage to download app insights blobs

---
+++ BlobExporter/BlobStorageDownloader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace BlobExporter
+{
+    public class BlobStorageDownloader
+    {
+        private readonly string _containerName;
+        private readonly string _connectionString;
+
+        public BlobStorageDownloader(string containerName)
+        {
+            _containerName = containerName;
+            _connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+        }
+
+        public IEnumerable<string> Import()
+        {
+            var storageAccount = CloudStorageAccount.Parse(_connectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var containerReference = blobClient.GetContainerReference(_containerName);
+
+            // use flat blob listing to reduce calls to API
+            foreach (var blob in containerReference.ListBlobs(useFlatBlobListing: true))
+            {
+                yield return DownloadBlob(blobClient, blob);
+            }
+        }
+
+        private static string DownloadBlob(CloudBlobClient blobClient, IListBlobItem blob)
+        {
+            var cloudBlockBlob = blobClient.GetBlobReferenceFromServer(blob.Uri);
+            using (var memoryStream = new MemoryStream())
+            {
+                cloudBlockBlob.DownloadToStream(memoryStream);
+                return Encoding.UTF8.GetString(memoryStream.ToArray());
+            }
+        }
+    }
+}
