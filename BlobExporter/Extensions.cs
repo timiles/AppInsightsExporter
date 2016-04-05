f33950f539f0147545a03363b36604d7dd2a0ac1
tim_tillr <tim@tillr.io>
2016-04-05 17:54:10 +0100

    tidier for when Assembly isn't populated, eg javascript exceptions

--- BlobExporter/Extensions.cs
+++ BlobExporter/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using BlobExporter.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BlobExporter
@@ -8,22 +9,30 @@ namespace BlobExporter
     {
         public static string ToFriendlyString(this IEnumerable<StackTraceLevel> stackTrace)
         {
+            var includeAssemblyInfo = stackTrace.Any(x => !string.IsNullOrWhiteSpace(x.Assembly));
+            var indentSize = includeAssemblyInfo ? 4 : 0;
+
             var sb = new StringBuilder();
 
             var previousLevel = new StackTraceLevel();
             foreach (var level in stackTrace)
             {
-                if (level.Assembly != previousLevel.Assembly)
+                if (includeAssemblyInfo)
                 {
-                    sb.AppendLine(level.Assembly);
+                    if (level.Assembly != previousLevel.Assembly)
+                    {
+                        sb.AppendLine(level.Assembly);
+                    }
+
+                    sb.Append(' ', indentSize);
                 }
 
-                sb.Append(" -- ");
                 sb.AppendLine(level.Method);
 
                 if (!string.IsNullOrEmpty(level.FileName))
                 {
-                    sb.AppendLine($"      ({level.FileName}:{level.LineNumber})");
+                    sb.Append(' ', indentSize + 2);
+                    sb.AppendLine($"({level.FileName} line:{level.LineNumber})");
                 }
 
                 previousLevel = level;
