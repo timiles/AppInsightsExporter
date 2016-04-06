5a3a48513c2360ebaac131535867a45ff81b9310
tim_tillr <tim@tillr.io>
2016-04-06 10:55:12 +0100

    Improve output format; store blob info; delete if applicable

--- SlackImporterApp/Program.cs
+++ SlackImporterApp/Program.cs
@@ -1,11 +1,16 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.IO;
 using BlobExporter;
+using BlobExporter.Models;
 using SlackImporter;
 
 namespace SlackImporterApp
 {
     class Program
     {
+        private const bool DeleteBlobsOnceImported = false;
+
         static void Main(string[] args)
         {
             var blobClient = new BlobExporterClient(new StorageConfiguration(), new RunTracker());
@@ -17,8 +22,24 @@ namespace SlackImporterApp
 
             foreach (var e in exceptions)
             {
-                slackClient.PostMessage($"{e.EventTime.ToString("F")}: {e.Message}");
+                slackClient.PostMessage($"{e.EventTime.ToString("F")} UTC: `{e.Message}` ```{e.StackTrace.ToFriendlyString()}```");
+
+                Console.WriteLine($"{e.EventTime.ToString("F")} UTC: {e.Message}");
+
+                StoreOriginalBlobInfo(e.OriginalBlobInfo);
+
+                if (DeleteBlobsOnceImported)
+                {
+                    blobClient.Delete(e.OriginalBlobInfo.Path);
+                }
             }
+        }
+
+        private static void StoreOriginalBlobInfo(BlobInfo blobInfo)
+        {
+            var path = "Originals/" + blobInfo.Path;
+            Directory.CreateDirectory(new FileInfo(path).Directory.FullName);
+            File.AppendAllText(path, blobInfo.Content);
         }
     }
 }
